ac2fb8c8be4ea66a17b3040af5be07ed473c88e6
yizhanzhang <yizhanzhang@tencent.com>
2022-08-05 13:43:38 +0800

    fix stop: mcLcd

--- SmallDesktopDisplay/src/mclcd/mclcd.cpp
+++ SmallDesktopDisplay/src/mclcd/mclcd.cpp
@@ -5,9 +5,10 @@ extern TFT_eSprite clk;
 
 extern int BL_addr;
 extern int Ro_addr;
-extern int LCD_Rotation;
 extern int LCD_BL_PWM;
 extern uint16_t bgColor;
+
+extern McWifi mcWifi;
 
 bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap) {
   if ( y >= tft.height() ) return 0;
@@ -17,28 +18,25 @@ bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap) 
 
 void McLcd::initLcd() {
   Serial.println("McLcd: initLcd");
-
-  // 背光亮度设置
-  if (EEPROM.read(BL_addr) > 0 && EEPROM.read(BL_addr) < 100)
-    LCD_BL_PWM = EEPROM.read(BL_addr);
-  // 屏幕方向设置
-  if (EEPROM.read(Ro_addr) >= 0 && EEPROM.read(Ro_addr) <= 3)
-    LCD_Rotation = EEPROM.read(Ro_addr);
-
   pinMode(LCD_BL_PIN, OUTPUT);
-  analogWrite(LCD_BL_PIN, 1023 - (LCD_BL_PWM * 10));
 
   tft.begin(); /* TFT init */
-  tft.invertDisplay(1);//反转所有显示颜色：1反转，0正常
-  tft.setRotation(LCD_Rotation);
+  tft.invertDisplay(1); //反转所有显示颜色：1反转，0正常
+  tft.setRotation(0); // 设定屏幕方向
   tft.fillScreen(0x0000);
   tft.setTextColor(TFT_BLACK, bgColor);
+
+  setBrightness(mcWifi.wifiConf.lcdBl);
 };
 
 void McLcd::setBrightness(int pwm) {
+  if (pwm <= 0 || pwm >= 100) {
+    pwm = 8;
+  }
   Serial.println("McLcd: setBrightness");
   analogWrite(LCD_BL_PIN, 1023 - (pwm * 10));
-  Serial.println("亮度调整为：" + LCD_BL_PWM);
+  Serial.print("亮度调整为：");
+  Serial.println(pwm);
 };
 
 void McLcd::initTJpgDec() {
