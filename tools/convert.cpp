5692a8141c5717f7e6564c210f169927e02c8271
yizhanzhang <yizhanzhang@tencent.com>
2023-04-21 14:21:19 +0800

    fix: update tools

--- tools/convert.cpp
+++ tools/convert.cpp
@@ -9,19 +9,44 @@
 
 using namespace std;
 
-void convertToByteCode(string filepath) {
-  struct stat pstat;
-  stat(filepath.c_str(), &pstat);
-  int size = pstat.st_size;
+string findJPGFilename(string filepath) {
+  int slashPos = filepath.rfind('/');
+  string fullFilename = filepath.substr(slashPos + 1);
 
+  int extPos = fullFilename.find(".jpg") > 0 ? fullFilename.find(".jpg") : fullFilename.find(".jpeg");
+  if (extPos == -1) return "";
+
+  string realFilename = fullFilename.substr(0, extPos);
+  return realFilename;
+}
+
+void convertToByteCode(string filepath) {
+  // check filename
+  string realFilename = findJPGFilename(filepath);
+  if (realFilename == "") {
+    cout << "[ERROR]: file name is invalid, must end with .jpg or .jpeg" << endl;
+    return;
+  };
+
+  // check file stat
+  struct stat pstat;
+  int status = stat(filepath.c_str(), &pstat);
+  if (status == -1) {
+    cout << "[ERROR]: file stat is invalid, please check filepath" << endl;
+    return;
+  }
+
+  // get image data
+  int size = pstat.st_size;
   FILE *fp = fopen(filepath.c_str(), "rb");
   uint8_t data[size];
   fread(data, sizeof(uint8_t), size, fp);
   fclose(fp);
 
+  // write header file
   string outFilepath = filepath + ".h";
   FILE *ofp = fopen(outFilepath.c_str(), "wb");
-  string output = "#include <pgmspace.h>\nconst uint8_t OUTPUT[] = {\n";
+  string output = "#include <pgmspace.h>\nconst uint8_t output_" + realFilename + "[] = {\n";
   for (int i = 0; i < size; i++) {
     std::ostringstream hex;
     hex << std::hex << (int)data[i];
@@ -40,9 +65,11 @@ void convertToByteCode(string filepath) {
 }
 
 int main(int argc, char* argv[]) {
-  if (argc < 2) return 0;
+  if (argc < 2) {
+    cout << "[ERROR]: no file path" << endl;
+    return 0;
+  };
   string filepath = argv[1];
-  if (!filepath) return;
   convertToByteCode(filepath);
   return 0;
 }
